50f5b356bc698d6cd1cc190bd81d0f33976b9809
Wasif Abbas <WasifAbbas900@gmail.com>
2022-09-12 13:22:41 +0530

    new changes  in drop down

--- src/assets/Table/Table.jsx
+++ src/assets/Table/Table.jsx
@@ -5,14 +5,14 @@ import Body from "./Body/Body";
 import Filter from "./Filter/Filter";
 import TableHeader from "./TableHeader/TableHeader";
 
-const Table = ({ header }) => {
+const Table = ({ header, setPage, page }) => {
   return (
     <>
       <Header />
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">Transactions</h5>
-          <Filter />
+          <Filter setPage={setPage} page={page} />
           <div className="wallet-history">
             <table className="table">
               <TableHeader header={header} />
